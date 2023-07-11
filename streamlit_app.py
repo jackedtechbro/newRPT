cbcf1e667ff448f4f4335254086d5c824bf226d3
jackedtechbro <139194810+jackedtechbro@users.noreply.github.com>
2023-07-10 20:57:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to the New Rice Purity Test! :heart:
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Edit `/streamlit_app.py` to customize this app to your heart's desire 
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+'''
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -36,3 +36,12 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+'''
+
+agree = st.checkbox('I agree')
+test = st.checkbox('test')
+temp = 0  
+temp += st.checkbox('test2')
+if agree:
+    st,write('temp = ' + temp)
+    st.write('Great!')
